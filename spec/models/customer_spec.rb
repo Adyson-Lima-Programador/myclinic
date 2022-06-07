e8874b2ee125e791705d8e7b3a9f4487f0df746c
Adyson-Lima-Programador <adyson_lima@yahoo.com.br>
2022-06-07 20:24:27 -0300

    4° commit - teste model customer GREEN

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Customer, type: :model do
 
   describe "Testa correto preenchimento dos campos do model Customer" do
 
-    it "nome consegue ser preenchido" do
+    it "name consegue ser preenchido" do
 
       @customer.name = "José Silva"
       expect(@customer.name).to eq("José Silva")
@@ -20,7 +20,7 @@ RSpec.describe Customer, type: :model do
 
     end
 
-    it "celular consegue ser preenchido" do
+    it "cell_phone consegue ser preenchido" do
 
       @customer.cell_phone = "5591923568945"
       expect(@customer.cell_phone).to eq("5591923568945")
@@ -40,7 +40,7 @@ RSpec.describe Customer, type: :model do
       expect(@customer.age).to eq("65")
 
     end
-    
+
   end
 
 end
