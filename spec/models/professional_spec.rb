15f714d2a8a5ce44fb503d7aa8f359375ec08449
Adyson-Lima-Programador <adyson_lima@yahoo.com.br>
2022-06-09 17:06:24 -0300

    6° commit - test model professional RED

---
+++ spec/models/professional_spec.rb
@@ -0,0 +1,46 @@
+require "rails_helper"
+
+RSpec.describe Professional, type: :model do
+
+  before {@professional = Professional.new}
+
+  describe "Testa correto preenchimento dos campos do model Professional" do
+
+    it "name consegue ser preenchido" do
+
+      @professional.name = "José Silva"
+      expect(@professional.name).to eq("José Silva")
+
+    end
+
+    it "email consegue ser preenchido" do
+
+      @professional.email = "jose@gmail.com"
+      expect(@professional.email).to eq("jose@gmail.com")
+
+    end
+
+    it "cell_phone consegue ser preenchido" do
+
+      @professional.cell_phone = "5591923568945"
+      expect(@professional.cell_phone).to eq("5591923568945")
+
+    end
+
+    it "cpf consegue ser preenchido" do
+
+      @professional.cpf = "65236985478"
+      expect(@professional.cpf).to eq("65236985478")
+
+    end
+
+    it "specialty consegue ser preenchido" do
+
+      @professional.specialty = "cardiologista"
+      expect(@professional.specialty).to eq("cardiologista")
+
+    end
+
+  end
+  
+end
