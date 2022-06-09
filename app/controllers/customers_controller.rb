91170cd1fd20180c73a4d173d31069e10ee1660e
Adyson-Lima-Programador <adyson_lima@yahoo.com.br>
2022-06-09 18:48:54 -0300

    14° commit - teste controller customers REFACTOR

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,6 +3,38 @@ class CustomersController < ApplicationController
   before_action :set_customer, only: %i[show edit update destroy]
 
   def index
+
+    if(params[:cpf].to_s != "")
+
+      customer_find = Customer.where(cpf: params[:cpf])
+      @customers = Customer.where(id: customer_find).page(params[:page]).per(5)
+      
+    else
+      
+      @customers = Customer.order(:id).page(params[:page]).per(10)
+    
+    end
+
+    if !params[:cpf]
+
+      @customers = Customer.order(:id).page(params[:page]).per(10)
+    
+    end  
+
+  end
+
+private
+
+  def set_customer
+
+    @customer = Customer.find(params[:id])
+
+  end
+
+  def customer_params
+
+    params.require(:customer).permit(:name, :email, :cell_phone, :cpf, :age)
+
   end
 
 end
